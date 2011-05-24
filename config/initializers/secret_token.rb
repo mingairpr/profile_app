bb80da792a9e8f9a7a752fcd80fd21d3e0fea019
Ming Loong Chng <mchng@princeton.edu>
2011-05-24 12:13:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfileApp::Application.config.secret_token = '7278ada731b75327a98c87ab90324dd6cf2ef43f26470992b5b14bf53b80c60179f3c726aaa9988a01c449bd56c1e0a4e0e2a78e97f72aa681572a3df1e0f249'
